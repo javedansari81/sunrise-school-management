05e64332eea7759fa14139d290cf9e25bce957fb
javedansari81 <javed.ansari81@gmail.com>
2025-07-29 15:25:41 +0530

    minor changes

--- sunrise-backend-fastapi/app/api/v1/api.py
+++ sunrise-backend-fastapi/app/api/v1/api.py
@@ -13,7 +13,3 @@ api_router.include_router(students.router, prefix="/students", tags=["students"]
 api_router.include_router(leaves.router, prefix="/leaves", tags=["leaves"])
 api_router.include_router(expenses.router, prefix="/expenses", tags=["expenses"])
 api_router.include_router(fees.router, prefix="/fees", tags=["fees"])
-api_router.include_router(products.router, prefix="/products", tags=["products"])
-api_router.include_router(classes.router, prefix="/classes", tags=["classes"])
-api_router.include_router(events.router, prefix="/events", tags=["events"])
-api_router.include_router(testimonials.router, prefix="/testimonials", tags=["testimonials"])
